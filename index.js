cae209bd896d8203e6f8f929bf3ffbf3ade8fd53
Daegan Wilcox <daeganwilcox@Daegans-MacBook-Pro.local>
2020-10-24 20:55:40 -0400

    Create node.js files

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+/*jshint esversion: 6 */
 
+const express = require("express");
+const http = require("http");
+const path = require("path");
+
+app = express();
+app.use(express.json);
+app.use(express.static("express"));
+
+app.use('/', function(req, res) {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+const server = http.createServer(app);
+const port = 3000;
+server.listen(port);
